c3d9bfb3ac21a228bf6ded3a5566339e0c2e1655
htndev <he4then.mail@gmail.com>
2021-02-24 00:24:09 +0200

    chore: apollo client generator added

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,16 @@
 export const PROJECT_TITLE = 'xBeat';
+
+export const FIELD_LENGTH = {
+  EMAIL: {
+    MIN: 6,
+    MAX: 255
+  },
+  USERNAME: {
+    MIN: 2,
+    MAX: 24
+  },
+  PASSWORD: {
+    MIN: 8,
+    MAX: 255
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './constants';
 export * from './endpoint';
 export * from './http-status';
 export * from './object';
+export * from './apollo';
